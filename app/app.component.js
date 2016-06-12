80e331560cc61d86b7cdd60a86a141719bb3692f
Abhilash Shrivastava <abhilash.shrivastava@hotmail.com>
2016-06-12 01:39:55 -0700

    login implemented

--- app/app.component.js
+++ app/app.component.js
@@ -16,14 +16,37 @@ var router_deprecated_1 = require('@angular/router-deprecated');
 var service_provider_component_1 = require("./service-provider/service-provider.component");
 var parcel_sender_component_1 = require("./parcel-sender/parcel-sender.component");
 var service_provider_crud_service_1 = require('./services/service-provider-crud.service');
+var angular2_jwt_1 = require('angular2-jwt');
 var AppComponent = (function () {
     function AppComponent() {
         this.title = 'Meet The Need';
+        this.lock = new Auth0Lock('LRhhXTSa1WjZFnbW0uDzH2GDtWucVlnJ', 'abhilashshrivastava.auth0.com');
+        this.jwtHelper = new angular2_jwt_1.JwtHelper();
     }
+    AppComponent.prototype.logout = function () {
+        localStorage.removeItem('profile');
+        localStorage.removeItem('id_token');
+        this.loggedIn();
+    };
+    AppComponent.prototype.login = function () {
+        var _this = this;
+        this.lock.show(function (err, profile, id_token) {
+            if (err) {
+                throw new Error(err);
+            }
+            localStorage.setItem('profile', JSON.stringify(profile));
+            localStorage.setItem('id_token', id_token);
+            console.log(_this.jwtHelper.decodeToken(id_token), _this.jwtHelper.getTokenExpirationDate(id_token), _this.jwtHelper.isTokenExpired(id_token));
+            _this.loggedIn();
+        });
+    };
+    AppComponent.prototype.loggedIn = function () {
+        return angular2_jwt_1.tokenNotExpired();
+    };
     AppComponent = __decorate([
         core_1.Component({
             selector: 'my-app',
-            template: "\n                <h1>{{title}}</h1>\n                <nav>\n                <a [routerLink]=\"['ServiceProvider']\">Service Provider</a>\n                <a [routerLink]=\"['ParcelSender']\">Parcel Sender</a>\n                </nav>\n                <router-outlet></router-outlet>        ",
+            template: "\n                <h1>{{title}}</h1>\n                <nav>\n                <a [routerLink]=\"['ServiceProvider']\">Service Provider</a>\n                <a [routerLink]=\"['ParcelSender']\">Parcel Sender</a>\n                <button *ngIf=\"!loggedIn()\" (click)=\"login()\">Login</button>\n                <button *ngIf=\"loggedIn()\" (click)=\"logout()\">Logout</button>\n                </nav>\n                <router-outlet></router-outlet>        ",
             styleUrls: ['app/app.component.css'],
             directives: [router_deprecated_1.ROUTER_DIRECTIVES],
             providers: [
--- app/app.component.ts
+++ app/app.component.ts
@@ -7,7 +7,9 @@ import { RouteConfig, ROUTER_DIRECTIVES, ROUTER_PROVIDERS } from '@angular/route
 import {ServiceProviderComponent} from "./service-provider/service-provider.component";
 import {ParcelSenderComponent} from "./parcel-sender/parcel-sender.component";
 import { ServiceProviderCRUDService }         from './services/service-provider-crud.service';
+import {tokenNotExpired, JwtHelper} from 'angular2-jwt';
 
+declare var Auth0Lock;
 
 @Component({
     selector: 'my-app',
@@ -16,6 +18,8 @@ import { ServiceProviderCRUDService }         from './services/service-provider-
                 <nav>
                 <a [routerLink]="['ServiceProvider']">Service Provider</a>
                 <a [routerLink]="['ParcelSender']">Parcel Sender</a>
+                <button *ngIf="!loggedIn()" (click)="login()">Login</button>
+                <button *ngIf="loggedIn()" (click)="logout()">Logout</button>
                 </nav>
                 <router-outlet></router-outlet>        `,
     styleUrls: ['app/app.component.css'],
@@ -41,5 +45,37 @@ import { ServiceProviderCRUDService }         from './services/service-provider-
 ])
 
 export class AppComponent {
-    title = 'Meet The Need'
+    title = 'Meet The Need';
+
+    lock = new Auth0Lock('LRhhXTSa1WjZFnbW0uDzH2GDtWucVlnJ', 'abhilashshrivastava.auth0.com');
+    jwtHelper = new JwtHelper();
+
+    logout() {
+
+        localStorage.removeItem('profile');
+        localStorage.removeItem('id_token');
+        this.loggedIn();
+    }
+
+    login() {
+
+        this.lock.show((err: string, profile: string, id_token: string) => {
+            if (err){
+                throw new Error(err);
+            }
+
+            localStorage.setItem('profile', JSON.stringify(profile));
+            localStorage.setItem('id_token', id_token);
+
+            console.log(
+                this.jwtHelper.decodeToken(id_token),
+                this.jwtHelper.getTokenExpirationDate(id_token),
+                this.jwtHelper.isTokenExpired(id_token)
+            );
+            this.loggedIn();
+        })
+    }
+    loggedIn() {
+        return tokenNotExpired();
+    }
 }
