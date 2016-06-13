8c68d856bd62faaf95b76991ae48b62455446b79
Abhilash Shrivastava <abhilash.shrivastava@hotmail.com>
2016-06-12 20:43:47 -0700

    form part done for service provider

--- app/service-provider/service-provider.component.js
+++ app/service-provider/service-provider.component.js
@@ -15,13 +15,33 @@ var core_1 = require('@angular/core');
 var router_deprecated_1 = require('@angular/router-deprecated');
 var service_provider_details_1 = require('./../services/service-provider-details');
 var service_provider_crud_service_1 = require('./../services/service-provider-crud.service');
+//var fetch = require('node-fetch');
 var ServiceProviderComponent = (function () {
     function ServiceProviderComponent(serviceProviderCRUDService, routeParams) {
         this.serviceProviderCRUDService = serviceProviderCRUDService;
         this.routeParams = routeParams;
+        //model = new ServiceProvider('Abhilash', 'abhilash.shrivastava@hotmail.com', 2485679221, {'50 Chumasero Drive', 'Apt 4k', 'San Francisco', 'CA'}, {'50 Chumasero Drive', 'Apt 4k', 'San Francisco', 'CA'}, '15 Jun 2016', {'8 PM', '10 PM'}, 20, 10, 10, 10);
+        this.model = new service_provider_details_1.ServiceProviderDetails();
+        this.submitted = false;
         this.close = new core_1.EventEmitter();
         this.navigated = false; // true if navigated here
     }
+    ServiceProviderComponent.prototype.onSubmit = function () {
+        this.submitted = true;
+        console.log(this.model);
+        // fetch('http://localhost:9000/service-confirm', { method: 'POST', body: this.model })
+        //     .then(function(res) {
+        //         return res.json();
+        //     }).then(function(json) {
+        //     console.log(json);
+        // });
+    };
+    Object.defineProperty(ServiceProviderComponent.prototype, "diagnostic", {
+        // TODO: Remove this when we're done
+        get: function () { return JSON.stringify(this.model); },
+        enumerable: true,
+        configurable: true
+    });
     ServiceProviderComponent.prototype.ngOnInit = function () {
         var _this = this;
         if (this.routeParams.get('id') !== null) {
@@ -56,8 +76,7 @@ var ServiceProviderComponent = (function () {
         core_1.Component({
             selector: 'service-provider',
             templateUrl: 'app/service-provider/service-provider.component.html',
-            styleUrls: ['app/service-provider/service-provider.component.css'],
-            providers: [service_provider_crud_service_1.ServiceProviderCRUDService]
+            styleUrls: ['app/service-provider/service-provider.component.css']
         }), 
         __metadata('design:paramtypes', [service_provider_crud_service_1.ServiceProviderCRUDService, router_deprecated_1.RouteParams])
     ], ServiceProviderComponent);
--- app/service-provider/service-provider.component.ts
+++ app/service-provider/service-provider.component.ts
@@ -2,22 +2,37 @@
  * Created by Abhi on 6/11/16.
  */
 
+import { NgForm }    from '@angular/common';
 import { Router } from '@angular/router-deprecated';
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { RouteParams } from '@angular/router-deprecated';
 
 import { ServiceProviderDetails }        from './../services/service-provider-details';
 import { ServiceProviderCRUDService } from './../services/service-provider-crud.service';
+//var fetch = require('node-fetch');
+
 
 @Component({
     selector: 'service-provider',
     templateUrl: 'app/service-provider/service-provider.component.html',
-    styleUrls: ['app/service-provider/service-provider.component.css'],
-    providers: [ServiceProviderCRUDService]
-
+    styleUrls: ['app/service-provider/service-provider.component.css']
 })
 
 export class ServiceProviderComponent {
+    //model = new ServiceProvider('Abhilash', 'abhilash.shrivastava@hotmail.com', 2485679221, {'50 Chumasero Drive', 'Apt 4k', 'San Francisco', 'CA'}, {'50 Chumasero Drive', 'Apt 4k', 'San Francisco', 'CA'}, '15 Jun 2016', {'8 PM', '10 PM'}, 20, 10, 10, 10);
+    model = new ServiceProviderDetails();
+    submitted = false;
+    onSubmit() { this.submitted = true;
+        console.log(this.model);
+        // fetch('http://localhost:9000/service-confirm', { method: 'POST', body: this.model })
+        //     .then(function(res) {
+        //         return res.json();
+        //     }).then(function(json) {
+        //     console.log(json);
+        // });
+    }
+    // TODO: Remove this when we're done
+    get diagnostic() { return JSON.stringify(this.model); }
     @Input() serviceProviderDetails: ServiceProviderDetails;
     @Output() close = new EventEmitter();
     error: any;
